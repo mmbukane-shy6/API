874d9671f85b6b3ea2ef97a002d3ce28eaf204cf
mmbukane-shy6 <sheilaegeidza@gmail.com>
2020-06-10 00:04:20 +0300

    created app called myapp

--- awwards/wsgi.py
+++ awwards/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for awward project.
+WSGI config for awwards project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "awward.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "awwards.settings")
 
 application = get_wsgi_application()
