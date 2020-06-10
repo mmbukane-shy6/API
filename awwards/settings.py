07617f4ff1960cc3f0ce6824489c01f3c1985f2f
mmbukane-shy6 <sheilaegeidza@gmail.com>
2020-06-10 11:35:26 +0300

    installed bootsrap

--- awwards/settings.py
+++ awwards/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
+    'bootstrap3',
     'crispy_forms',
     'pyuploadcare.dj',
 ]
